e1713c40950c6f2e47bbb44b778df72875e2c4f5
romain <romain.esquerra@oclock.school>
2024-07-21 17:48:32 +0200

    bientot la fin

--- assets/js/main.js
+++ assets/js/main.js
@@ -60,10 +60,10 @@ function navHighlighter(){
 const filterContainer= document.querySelector('.portfolio-filter-inner'),
       filterBtns = filterContainer.children,
       totalFilterBtn = filterBtns.length,
-      portfolioItems = document.querySelectorAll(".portfolio-item")
+      portfolioItems = document.querySelectorAll(".portfolio-item"),
       totalPortfolioItem = portfolioItems.length;
 
-      for(let i=0; i<totalFilterBtn; i++){
+      for(let i=0; i < totalFilterBtn; i++){
         filterBtns[i].addEventListener("click",function(){
             filterContainer.querySelector(".active").classList.remove('active')
             this.classList.add("active");
@@ -84,6 +84,7 @@ const filterContainer= document.querySelector('.portfolio-filter-inner'),
                 }
             }
         })
+        window.dispatchEvent(new Event('resize'));
       }
 /*=============== THEME/DISPLAY CUSTOMIZATION ===============*/
 const theme = document.querySelector("#theme-button");
